97dacf4a3dade9e23b7d5163baf3d8d432751272
bw-lc <bw9481076>
2017-03-11 19:05:01 +0800

    xfzxzvc

--- demo4/cookbook/c3/cookbook.js
+++ demo4/cookbook/c3/cookbook.js
@@ -193,13 +193,12 @@ function cb_ok(url) {
 
 var gcb_cmds = [
 {"cmd": ">set_c 0,2,s,220,1;", "swiper_index": 4, "start_js": "gcb_media.pause()", "end_js": "cb_media_play()"},
-{"cmd": ">set_c 1,3,n,180,360;", "swiper_index": 5, "start_js": "gcb_media.pause()", "end_js": ""},
-{"cmd": ">set_c 2,3,s,180,1;", "swiper_index": 6, "start_js": "", "end_js": "cb_media_play()"},
+{"cmd": ">set_c 1,3,s,180,380;", "swiper_index": 5, "start_js": "gcb_media.pause()", "end_js": "cb_media_play()"},
 
-{"cmd": ">set_c 3,3,n,180,360;", "swiper_index": 7, "start_js": "gcb_media.pause()", "end_js": "cb_media_play()"},
-{"cmd": ">set_c 4,4,n,0,1;", "swiper_index": 8, "start_js": "", "end_js": "cb_media_play()"},
+{"cmd": ">set_c 2,3,n,180,360;", "swiper_index": 6, "start_js": "gcb_media.pause()", "end_js": ""},
+{"cmd": ">set_c 3,4,n,0,1;", "swiper_index": 7, "start_js": "", "end_js": "cb_media_play()"},
 
-{"cmd": ">run_c;", "swiper_index": 9, "start_js": "", "end_js": ""}
+{"cmd": ">run_c;", "swiper_index": 8, "start_js": "", "end_js": ""}
 ];
 
 
