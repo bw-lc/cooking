2db4fa01994d952a0b34faa37ba71f14471c3b79
bw-lc <bw9481076>
2017-04-30 08:51:01 +0800

    gfkjfcgf

--- demo4/cookbook/manual-cook/cookbook.js
+++ demo4/cookbook/manual-cook/cookbook.js
@@ -129,7 +129,7 @@ function cb_showerr(s, lv) {
 
 var cb_statusText = $('statusText');
 function cb_showinfo_none() {
-  cb_statusText.innerHTML = '';
+  cb_statusText.innerHTML = '.';
 }
 
 function cb_showinfo(str, lv, ms) {
